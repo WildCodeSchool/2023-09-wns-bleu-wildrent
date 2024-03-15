e7ed3880699fbd4bd5d73c52938db6d88fdeadf5
Noiramd31 <marion2macedo@gmail.com>
2024-03-15 09:14:07 +0100

    test ci

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,6 +16,7 @@ export default function Navbar() {
   const isLoggedIn = checkUserIsLoggedIn();
   const isAdmin = checkUserIsLoggedIn() === 'ADMIN';
   const avatar = data?.getProfile?.picture;
+  const name = data?.getProfile?.lastname;
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -76,10 +77,10 @@ export default function Navbar() {
               <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full" data-test-id="avatar">
                   <img
-                    alt="Tailwind CSS Navbar component"
+                    alt={name}
                     src={
                       avatar ||
-                      'https://lachroniquefacile.fr/wp-content/uploads/2018/07/%EF%BC%9F.png'
+                      'https://st3.depositphotos.com/3581215/18899/v/450/depositphotos_188994514-stock-illustration-vector-illustration-male-silhouette-profile.jpg'
                     }
                   />
                 </div>
