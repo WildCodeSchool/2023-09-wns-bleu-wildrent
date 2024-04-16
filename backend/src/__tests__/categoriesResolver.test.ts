c36d594649bf124ab94d9eee129a92f58845011b
Noiramd31 <marion2macedo@gmail.com>
2024-04-16 13:54:45 +0200

    test CI build

--- backend/src/__tests__/categoriesResolver.test.ts
+++ backend/src/__tests__/categoriesResolver.test.ts
@@ -1,50 +1,52 @@
-// import { execute } from '../../jest.setup';
-// import { Category } from '../entities/category.entity';
-// // import getAdminContext from './helpers/getAdminContext';
-// import getCategories from './operations/getCategories';
-// import getCategoryById from './operations/getCategoryById';
+import { execute } from '../../jest.setup';
+import { Category } from '../entities/category.entity';
+// import getAdminContext from './helpers/getAdminContext';
+import getCategories from './operations/getCategories';
+import getCategoryById from './operations/getCategoryById';
 
-// describe('Categories Resolver', () => {
-//   it('can get a list of categories', async () => {
-//     await Category.create({
-//       name: 'Mobilier',
-//       description:
-//         'Oat cake biscuit cookie chocolate pudding. Fruitcake cake cupcake bonbon pudding tiramisu jelly beans marzipan. Shortbread dessert chocolate biscuit jelly-o brownie. Toffee tootsie roll lollipop jelly beans powder croissant pie. Sesame snaps jelly-o dessert sugar plum donut caramels chocolate. Bear claw macaroon brownie cookie icing jelly sugar plum.',
-//       image: 'https://www.highgest.fr/image/562535501368/1920/928/Les-petits-+.jpg',
-//     }).save();
-//     await Category.create({
-//       name: 'Eclairage',
-//       description:
-//         'Oat cake biscuit cookie chocolate pudding. Fruitcake cake cupcake bonbon pudding tiramisu jelly beans marzipan. Shortbread dessert chocolate biscuit jelly-o brownie. Toffee tootsie roll lollipop jelly beans powder croissant pie. Sesame snaps jelly-o dessert sugar plum donut caramels chocolate. Bear claw macaroon brownie cookie icing jelly sugar plum.',
-//       image: 'https://www.highgest.fr/image/505818799390/1920/945/Les-petits-+.jpg',
-//     }).save();
-//     const res = await execute(getCategories);
-//     expect(res).toMatchSnapshot();
-//   });
+describe('Categories Resolver', () => {
+  it('can get a list of categories', async () => {
+    await Category.create({
+      id: 1,
+      name: 'Mobilier',
+      description:
+        'Oat cake biscuit cookie chocolate pudding. Fruitcake cake cupcake bonbon pudding tiramisu jelly beans marzipan. Shortbread dessert chocolate biscuit jelly-o brownie. Toffee tootsie roll lollipop jelly beans powder croissant pie. Sesame snaps jelly-o dessert sugar plum donut caramels chocolate. Bear claw macaroon brownie cookie icing jelly sugar plum.',
+      image: 'https://www.highgest.fr/image/562535501368/1920/928/Les-petits-+.jpg',
+    }).save();
+    await Category.create({
+      id: 2,
+      name: 'Eclairage',
+      description:
+        'Oat cake biscuit cookie chocolate pudding. Fruitcake cake cupcake bonbon pudding tiramisu jelly beans marzipan. Shortbread dessert chocolate biscuit jelly-o brownie. Toffee tootsie roll lollipop jelly beans powder croissant pie. Sesame snaps jelly-o dessert sugar plum donut caramels chocolate. Bear claw macaroon brownie cookie icing jelly sugar plum.',
+      image: 'https://www.highgest.fr/image/505818799390/1920/945/Les-petits-+.jpg',
+    }).save();
+    const res = await execute(getCategories);
+    expect(res).toMatchSnapshot();
+  });
 
-//   it('can get one category by id', async () => {
-//     const cat3 = await Category.create({
-//       name: 'Sonorisation',
-//       description: 'Description de la catégorie Sonorisation',
-//       image: 'https://exemple.com/image_sonorisation.jpg',
-//     }).save();
-//     const res = await execute(getCategoryById, { categoryId: cat3.id });
-//     expect(res).toMatchSnapshot();
-//   });
+  it('can get one category by id', async () => {
+    const cat3 = await Category.create({
+      name: 'Sonorisation',
+      description: 'Description de la catégorie Sonorisation',
+      image: 'https://exemple.com/image_sonorisation.jpg',
+    }).save();
+    const res = await execute(getCategoryById, { categoryId: cat3.id });
+    expect(res).toMatchSnapshot();
+  });
 
-//   // it('can create a category', async () => {
-//   //   const res = await execute(
-//   //     addCategory,
-//   //     {
-//   //       data: {
-//   //         name: 'Bougie',
-//   //         description:
-//   //           'Oat cake biscuit cookie chocolate pudding. Fruitcake cake cupcake bonbon pudding tiramisu jelly beans marzipan. Shortbread dessert chocolate biscuit jelly-o brownie. Toffee tootsie roll lollipop jelly beans powder croissant pie. Sesame snaps jelly-o dessert sugar plum donut caramels chocolate. Bear claw macaroon brownie cookie icing jelly sugar plum.',
-//   //         image: 'https://www.highgest.fr/image/505818799390/1920/945/Les-petits-+.jpg',
-//   //       },
-//   //     },
-//   //     // await getAdminContext()
-//   //   );
-//   //   expect(res).toMatchSnapshot();
-//   // });
-// });
+  // it('can create a category', async () => {
+  //   const res = await execute(
+  //     addCategory,
+  //     {
+  //       data: {
+  //         name: 'Bougie',
+  //         description:
+  //           'Oat cake biscuit cookie chocolate pudding. Fruitcake cake cupcake bonbon pudding tiramisu jelly beans marzipan. Shortbread dessert chocolate biscuit jelly-o brownie. Toffee tootsie roll lollipop jelly beans powder croissant pie. Sesame snaps jelly-o dessert sugar plum donut caramels chocolate. Bear claw macaroon brownie cookie icing jelly sugar plum.',
+  //         image: 'https://www.highgest.fr/image/505818799390/1920/945/Les-petits-+.jpg',
+  //       },
+  //     },
+  //     // await getAdminContext()
+  //   );
+  //   expect(res).toMatchSnapshot();
+  // });
+});
