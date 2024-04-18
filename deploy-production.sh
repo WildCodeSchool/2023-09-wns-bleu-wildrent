30442ae8c7b1bd708194042ecb5f7102320d0033
Yohan Fabre <yohan.fabre@drapo.com>
2024-04-18 16:59:58 +0200

    part2

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+git fetch origin && git reset --hard origin/main && git clean -f -d && \
 docker compose -f docker-compose.production.yml down && \
-    docker compose -f docker-compose.production.yml pull && \
-    GATEWAY_PORT=8001 docker compose -f docker-compose.production.yml up -d;
+docker compose -f docker-compose.production.yml pull && \
+docker compose -f docker-compose.production.yml --env-file .env.production up -d;
