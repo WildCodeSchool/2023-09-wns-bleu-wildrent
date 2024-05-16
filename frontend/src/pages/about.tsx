d5e1e427bbeecf7e091776701c7cd02d61250af4
BrianV87 <Brian.v87@hotmail.fr>
2024-05-16 14:18:28 +0200

    add page A propos de nous

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>À propos de nous</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
