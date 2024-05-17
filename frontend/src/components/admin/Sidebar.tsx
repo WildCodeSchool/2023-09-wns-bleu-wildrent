9120be70d0f920f23732e0e8b9bb67d09a564dd2
Dreyrk <lucas.rondepierre123@gmail.com>
2024-05-17 14:36:56 +0200

    navitems initialized via getStaticProps

--- frontend/src/components/admin/Sidebar.tsx
+++ frontend/src/components/admin/Sidebar.tsx
@@ -1,15 +1,15 @@
 // components/dashboard/Sidebar.tsx
 
-import { adminSideBarItems } from '@/const';
 import SideBarLink from './navigation/SideBarLink';
+import { LinkProps } from '@/types/props';
 
-const Sidebar = () => {
+const Sidebar = ({ navItems }: { navItems: LinkProps[] }) => {
   return (
     <div className="flex flex-col w-64 px-4 py-8 bg-white border-r dark:bg-gray-200 dark:border-gray-600 min-h-screen">
       <h2 className="text-3xl font-semibold text-gray-800 dark:text-white">Dashboard</h2>
       <div className="flex flex-col justify-between flex-1 mt-6">
         <nav>
-          {adminSideBarItems.map(({ href, text, testId }) => (
+          {navItems.map(({ href, text, testId }) => (
             <SideBarLink key={href} href={href} text={text} testId={testId} />
           ))}
           {/* Insérez d'autres liens dans /const/adminSidebarItems si nécessaire */}
--- frontend/src/components/admin/navigation/SideBarLink.tsx
+++ frontend/src/components/admin/navigation/SideBarLink.tsx
@@ -9,10 +9,11 @@ export default function SideBarLink({ href, text, testId }: LinkProps) {
   const isActive = (pathname: string) => router.pathname === pathname;
   return (
     <Link
+      data-test-id={testId}
       href={href}
       className={`${isActive(href) ? 'bg-base-300' : ''} flex items-center px-4 py-2 cursor-pointer`}
     >
-      <span data-test-id={testId}>{text}</span>
+      {text}
     </Link>
   );
 }
